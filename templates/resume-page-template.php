e7cf81f479de88f6d42976574bff76d930d9da40
Bearded Avenger <email@nickhaskins.com>
2013-11-14 11:49:17 -0600

    checkpoin

--- templates/resume-page-template.php
+++ templates/resume-page-template.php
@@ -14,14 +14,14 @@ do_action('ba_resume_page_before');
 						<div class="col-sm-3">
 							<img src="http://placekitten.com/600/400" class="img-responsive">
 						</div>
-						<div class="col-sm-6">
-							<h1 class="zmt">Unicorn Rider</h1>
-							<h3 class="zmt">Crime Fighting Superstar</h3>
-							<a href="#">email@info.com</a>
-							<a href="#">http://website.com</a>
-							<a class="muted" href="#">555-555-5555</a>
+						<div class="col-sm-6 resume-bio">
+							<h1 class="zmt resume-bio-title">Unicorn Rider</h1>
+							<h3 class="zmt resume-bio-tag">Crime Fighting Superstar</h3>
+							<a href="#" class="resume-bio-email">email@info.com</a>
+							<a href="#" class="resume-bio-site">http://website.com</a>
+							<a class="muted resume-bio-phone" href="#">555-555-5555</a>
 						</div>
-						<div class="col-sm-3">
+						<div class="col-sm-3 resume-social-wrap">
 							<ul class="resume-social unstyled">
 								<li><a href="#">tweeter</a></li>
 								<li><a href="#">tweeter</a></li>
@@ -35,11 +35,11 @@ do_action('ba_resume_page_before');
 					<main class="resume-section-main">
 
 						<!-- start objective -->
-						<section class="row resume-objective">
+						<section class="row resume-objective-wrap">
 							<div class="col-sm-3">
-								<h4 class="zmt">Objective</h4>
+								<h4 class="zmt resume-item-title">Objective</h4>
 							</div>
-							<div class="col-sm-9">
+							<div class="col-sm-9 resume-objective">
 								<p>Seeking expanded opportunies within select organizations where I can utilize my expertise to implement profit oriented results.</p>
 							</div>
 						</section>
@@ -48,16 +48,16 @@ do_action('ba_resume_page_before');
 						<hr />
 
 						<!-- start work experience wrap -->
-						<section class="row resume-work-experience">
+						<section class="row resume-work-wrap">
 							<div class="col-sm-3">
-								<h4 class="zmt">Experience</h4>
+								<h4 class="zmt resume-item-title">Experience</h4>
 							</div>
 							<div class="col-sm-9">
 
 								<!-- company -->
-								<div class="row">
+								<div class="row resume-company">
 									<div class="col-sm-9">
-										<h4 class="zmt">Senior Project Manager</h4>
+										<h4 class="zmt resume-company-title">Senior Project Manager</h4>
 										<h4><small>Web Design stuidios</small></h4>
 										<p>Did some shit here</p>
 									</div>
@@ -68,9 +68,9 @@ do_action('ba_resume_page_before');
 								<!-- end company -->
 
 								<!-- company -->
-								<div class="row">
+								<div class="row resume-company">
 									<div class="col-sm-9">
-										<h4 class="zmt">Senior Project Manager</h4>
+										<h4 class="zmt resume-company-title">Senior Project Manager</h4>
 										<h4><small>Web Design stuidios</small></h4>
 										<p>Did some shit here</p>
 									</div>
@@ -87,16 +87,14 @@ do_action('ba_resume_page_before');
 						<hr />
 
 						<!-- start github activity stream -->
-						<section class="row resume-github-stream">
+						<section class="row resume-github-wrap">
 							<div class="col-sm-3">
-								<h4 class="zmt">Github Activity</h4>
+								<h4 class="zmt resume-item-title">Github Activity</h4>
 							</div>
-							<div class="col-sm-9">
-
-								<!-- company -->
+							<div class="col-sm-9 github-stream-wrap">
 								<ul class="github-stream unstyled">
-								<?php echo ba_resume_page_github_feed( 'bearded-avenger', $excluded = array(), $count = 5, $include_css = true);?>
-							</ul>
+									<?php echo ba_resume_page_github_feed( 'bearded-avenger', $excluded = array(), $count = 5, $include_css = true);?>
+								</ul>
 							</div>
 						</section>
 						<!-- end github activity stream -->
@@ -104,23 +102,23 @@ do_action('ba_resume_page_before');
 						<hr />
 
 						<!-- start skillset wrap -->
-						<section class="row resume-skillset">
+						<section class="row resume-skillset-wrap">
 							<div class="col-sm-3">
-								<h4 class="zmt">Skills</h4>
+								<h4 class="zmt resume-item-title">Skills</h4>
 							</div>
-							<div class="col-sm-9">
+							<div class="col-sm-9 resume-skillset-list-wrap">
 
 								<!-- skillset 1 -->
 								<div class="row">
 									<div class="col-sm-6">
-										<ul class="unstyled">
+										<ul class="unstyled resume-skillset-list">
 											<li>skill</li>
 											<li>skill</li>
 											<li>skill</li>
 										</ul>
 									</div>
 									<div class="col-sm-6">
-										<ul class="unstyled">
+										<ul class="unstyled resume-skillset-list">
 											<li>skill</li>
 											<li>skill</li>
 											<li>skill</li>
@@ -136,17 +134,17 @@ do_action('ba_resume_page_before');
 						<hr />
 
 						<!-- start education wrap -->
-						<section class="row resume-education">
+						<section class="row resume-education-wrap">
 							<div class="col-sm-3">
-								<h4 class="zmt">Education</h4>
+								<h4 class="zmt resume-item-title">Education</h4>
 							</div>
 							<div class="col-sm-9">
 
 								<!-- skillset 1 -->
-								<div class="row">
+								<div class="row resume-education-item">
 									<div class="col-sm-12">
-										<h3 class="zmt">Harvard</h3>
-										<h4><small>Business Law</small> 4.0 GPA</h4>
+										<h3 class="zmt resume-education-title">Harvard</h3>
+										<h4 class="resume-education-tag"><small>Business Law</small> 4.0 GPA</h4>
 										<p>I was awesome</p>
 									</div>
 								</div>
